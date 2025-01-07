985adce07fb64ffcbffa345e44a7d25720132bc9
Adis Durakovic <git@mail.adisdurakovic.com>
2025-01-07 16:53:24 +0100

    fix: version

--- apps/frontend/pages/profile.vue
+++ apps/frontend/pages/profile.vue
@@ -1,5 +1,23 @@
 <template>
-	<div>
+	<div class="flex flex-row">
 		<ProfileTrakt />
+		<div class="card w-96 bg-base-100 shadow-xl">
+			<div class="card-body">
+				<h2 class="card-title">Server</h2>
+				<p>Version: {{ serverVersion }}</p>
+			</div>
+		</div>
 	</div>
 </template>
+
+<script setup lang="ts">
+	const serverVersion = ref('')
+
+	onMounted(async () => {
+		const data = await usePb().send('/-/health', {
+			method: 'get',
+			cache: 'no-cache',
+		})
+		serverVersion.value = data.version
+	})
+</script>
