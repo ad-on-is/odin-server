52b67227f839be5a18210d5f25a321538814abd2
Adis Durakovic <git@mail.adisdurakovic.com>
2026-05-16 19:51:49 +0200

    changes

--- apps/backend/imdb/imdb.go
+++ apps/backend/imdb/imdb.go
@@ -2,34 +2,72 @@ package imdb
 
 import (
 	"encoding/json"
-	"strings"
+	"fmt"
+	"os"
 	"time"
 
 	"github.com/charmbracelet/log"
 	"github.com/go-resty/resty/v2"
 )
 
-func Get(id string) map[string]interface{} {
+type Item struct {
+	Rating float64 `json:"rating"`
+	Votes  int     `json:"votes"`
+}
+
+type ImdbItem struct {
+	AggregateRating struct {
+		RatingValue float64 `json:"ratingValue"`
+		RatingCount int     `json:"ratingCount"`
+	} `json:"aggregateRating"`
+	Review struct {
+		ReviewBody   string `json:"reviewBody"`
+		ReviewRating int    `json:"reviewRating"`
+	} `json:"review"`
+}
+
+type Reviews []struct {
+	Summary string `json:"summary"`
+	Text    string `json:"text"`
+	Rating  int    `json:"authorRating"`
+}
+
+func Get(id string) ImdbItem {
+	url := os.Getenv("IMDBAPI_URL")
 	request := resty.New().
 		SetRetryCount(3).
 		SetTimeout(time.Second * 30).
 		SetRetryWaitTime(time.Second).
 		R()
 
-	res, err := request.Get("https://www.imdb.com/title/" + id)
-	r := make(map[string]interface{})
+	endpoint := fmt.Sprintf("%s/movie/%s", url, id)
+	res, err := request.Get(endpoint)
+	i := Item{}
+	r := ImdbItem{}
 	if err == nil {
-		b := string(res.Body())
-		start := "<script type=\"application/ld+json\">"
-		end := "</script>"
-		startIndex := strings.Index(b, start)
-		endIndex := strings.Index(b[startIndex+len(start):], end)
-		str := b[startIndex+len(start) : endIndex+startIndex+len(start)]
-		if err := json.Unmarshal([]byte(str), &r); err != nil {
-			log.Error("imdb", "id", id, "err", err)
+		err = json.Unmarshal(res.Body(), &i)
+		if err != nil {
+			log.Error("Failed to unmarshal response", "error", err)
 		} else {
-			log.Info("imdb", "id", id, "parsed", "success")
+			r.AggregateRating.RatingValue = i.Rating
+			r.AggregateRating.RatingCount = i.Votes
+		}
+
+	}
+
+	endpoint = fmt.Sprintf("%s/reviews/%s", url, id)
+	res, err = request.Get(endpoint)
+
+	if err == nil {
+		var reviews Reviews
+		err = json.Unmarshal(res.Body(), &reviews)
+		if err != nil || len(reviews) == 0 {
+			log.Error("Failed to unmarshal response", "error", err)
+		} else {
+			r.Review.ReviewBody = fmt.Sprintf("%s. %s", reviews[0].Summary, reviews[0].Text)
+			r.Review.ReviewRating = reviews[0].Rating
 		}
 	}
+
 	return r
 }
